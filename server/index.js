536421da8ad4a363376abb4df3af1cd557b8e1e3
omarelloumi <48672001+omarelloumi@users.noreply.github.com>
2023-03-17 12:41:30 +0100

    Update index.js

--- server/index.js
+++ server/index.js
@@ -61,17 +61,6 @@ app.post('/getRepos', async (req, res) =>{
     });
 });
 
-if (process.env.NODE_ENV === 'production') {
-    // Serve any static files
-    app.use(express.static(path.join(__dirname, 'client/dist')));
-    app.get('*', (req, res) =>
-    res.sendFile(
-      path.resolve(__dirname, 'client', 'dist', 'index.html')
-    )
-  );
-}
-
-
 app.listen(PORT, function () {
     console.log('Listening on port ' + PORT);
 });
